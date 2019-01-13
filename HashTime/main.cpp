3c444c2ebbe40902e0f12aecd5a47fb589f5356d
Einstein Zheng <mocoder@icloud.com>
2019-01-13 17:31:22 +0800

    Initial Commit

---
+++ HashTime/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HashTime
+//
+//  Created by Einstein Zheng on 1/13/19.
+//  Copyright © 2019 Einstein Zheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
